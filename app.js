279ee099db222f783af00d4e40986b6c4ec3a570
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-03-23 14:49:28 -0500

    Third Commit. Added Greeting Function.

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 function askname() {
-    var name = prompt("What's ya name?")
+    var name = prompt("Hey buddy! How's it going! Whats ya name?")
+    return name
 }
 
-askname() //invoke
+function greeting(a) { // parameter
+    alert(`Well, welcome ${a}`)
+}
+
+greeting(askname()) // argument
+
+function conversion() {
+    var temp = prompt("Go ahead and type 'f' to convert from Fahrenheit to Celsius, and type 'c' to convert from Celsius to Fahrenheit.")
+    return temp
+}
+
+function math() {
+
+}
