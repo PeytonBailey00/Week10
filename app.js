fe04cb33ee392caee071a02986bf46125b96b0c8
Bailey <peybailey@FVFZMYE6J1WK.tld>
2022-03-25 14:20:36 -0500

    Fifth Commit. Finalized Code. Grade - 85.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function askname() {
     var name = prompt("Hey buddy! How's it going! Whats ya name?")
     return name
-}
+} //10
 
 function greeting(a) { // parameter
     alert(`Well, welcome to my Temperature Converter ${a}!`)
@@ -12,7 +12,7 @@ greeting(askname()) // argument
 function conversion() {
     var convert = prompt("Go ahead and type 'f' to convert from Fahrenheit to Celsius, and type 'c' to convert from Celsius to Fahrenheit.")
     return convert
-}
+} 
 
 console.log(conversion())
 
@@ -21,32 +21,25 @@ function temperature() {
     return temp
 }
 
+console.log(temperature())
+
 function fMath(x) {
     var temp = ((x - 32) * 5) / 9
     return temp
-}
+} // 20
+
+console.log(fMath())
 
 function cMath(y) {
     var temp = ((y / 5) * 9 ) + 32
     return temp
-}
+} //20
+
+console.log(cMath())
 
 function final() {
     alert(`Alrighty. It's ${temp}. You have a good day buddy.`)
 }
 
 
-
-
-
-
-
-
-
-
-
-if (convert == "f") {
-
-} else if (convert == "c") {
-
-}
+//85
